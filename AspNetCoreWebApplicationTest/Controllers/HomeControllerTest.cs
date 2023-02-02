0f1585c956e88364e077ee7f859a9e1c9e5e7c43
MSaifulKhai <81786072+MSaifulKhai@users.noreply.github.com>
2023-02-02 13:42:05 +0800

    Update HomeControllerTest.cs

--- AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
@@ -22,14 +22,6 @@ namespace AspNetCoreWebApplicationTest.Controllers
             ViewResult result = (ViewResult)controller.Error();
             Assert.Single(result.ViewData);
             Assert.Equal("We've encountered an error :(", result.ViewData["Message"]);
-        }
-        [Fact]
-        public void NoInputParamGetResponseTest()
-        {
-            HelloController controller = new HelloController();
-            var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!!!", response.output);
-        }
-        
+        }        
     }
 }
